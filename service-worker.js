15c07336133222d94e520b567c8ed0000a3c96b5
bashamcaleb530-hub <bashamcaleb530@gmail.com>
2026-09-08 20:25:38 -0700

    Add service worker for caching app files

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "biblemastery-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_FILES);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
