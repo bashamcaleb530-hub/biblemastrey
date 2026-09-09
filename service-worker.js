316dd50c180c0f67bb8976cb5aba8441047f0616
bashamcaleb530-hub <bashamcaleb530@gmail.com>
2026-09-08 21:14:21 -0700

    Update service worker cache name and logic
    Updated cache name and modified fetch handling for navigation requests.

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = "biblemastery-v2";
+const CACHE_NAME = "biblemastery-v4";
 
 const APP_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(APP_FILES);
@@ -16,10 +17,34 @@ self.addEventListener("install", event => {
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener("fetch", event => {
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match("/"))
+    );
+
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
